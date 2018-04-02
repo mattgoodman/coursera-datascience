1ecf59fc7de53ac22c57767cb04a69ec6b16fb35
mattgoodman <mgoodman@westpac.com.au>
2018-04-02 20:06:42 +1000

    update
    b

--- Week2Assesment/Week2Assesment/corr.R
+++ Week2Assesment/Week2Assesment/corr.R
@@ -1,4 +1,4 @@
-parsed <- function(directory, threshhold = 0) {
+parsed <- function(directory, threshold = 0) {
 
 	files <- list.files(directory, full.names = T)
 	# locate the files
@@ -9,11 +9,23 @@ parsed <- function(directory, threshhold = 0) {
 
 	# concatenate into one big data.frame
 	data.filtered <- complete.cases(data.cat)
+		
+	completeCases <- data.cat[data.filtered,]
 
-	data.cat[data.filtered,]
-	#completeCases <- data.cat[data.filtered,]
-
+	#completeCases
 	#completeCases[completeCases["Freq"] > threshold,]
 
 }
+
+complete <- function(directory, threshhold = 0) {
+
+	p <- parsed(directory, threshhold)
+
+	t <- as.data.frame(table(p$ID))
+
+	overThresh <- t[t["Freq"] > threshhold,]
+
+	valsOverThresh <- p[overThresh[[1]],]
+}
 #cor(data.cat$nitrate, data.cat$sulfate, use = "complete.obs")
+#sapply(split(v[2:4], v$ID), function(X) cor(X$sulfate, X$nitrate))
